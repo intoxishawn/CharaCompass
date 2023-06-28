058f0fb3484696e1fd54bf045e925126a060927a
Larissa Nicolini de Oliveira <02150129@aluno.canoas.ifrs.edu.br>
2023-06-28 14:19:51 -0300

    Classes de PHP parte 2

---
+++ CharaCompass/PHP/Personagem.class.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'Conexao.php';
+
+class Personagem{
+    private $id; 
+    private $nome;
+    private $idade;
+    private $sexo;
+    private $pronomes;
+    private $raca;
+    private $nacionalidade;
+    private $profissao;
+    private $importancia;
+    private $universo;
+    private $alinhamento;
+    private $tracos;
+    private $pts_fortes;
+    private $pts_fracos;
+    private $maneirismos;
+    private $social;
+    private $mentalidade;
+    private $passado;
+    private $grupo;
+    private $inimigos;
+    private $condicao_saude;
+    private $objetos;
+    private $trivias;
+
+    //getters e setters
+    
+
+}
+?>
