31b5afae848322a7695d71df570b3ac1ad8057b5
Shawn <99032697+intoxishawn@users.noreply.github.com>
2023-07-03 09:06:53 -0300

    Telas pt.3

---
+++ CharaCompass/JS/perfilUsuario.js
@@ -0,0 +1,27 @@
+function abreGaleria(){
+    window.location.href = "inicialUsuario.html";
+}
+
+function abrePerfilUsuario(){
+    window.location.href = "PerfilUsuario.html";
+}
+
+function abrePersonagens(){
+    window.location.href = "personagensUsuario.html";
+}
+
+function abreMundos(){
+    window.location.href = "mundosUsuario.html";
+}
+
+function abreObjetos(){
+    window.location.href = "objetosUsuario.html";
+}
+
+function abreEditar(){
+    window.location.href = "editar_Usuario.html";
+}
+
+function desconectar(){
+    window.location.href = "index.html";
+}
