3748e6e5b6ec33c4bc0b787dcb659c99e9ef9a74
Larissa Nicolini de Oliveira <02150129@aluno.canoas.ifrs.edu.br>
2023-08-07 16:04:28 -0300

    Tinha que adicionar no htdocs animal

--- CharaCompass/PHP/cadastrarCliente.php
+++ CharaCompass/PHP/cadastrarCliente.php
@@ -4,9 +4,9 @@ $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
 if ($acao === 'cadastrar') {
     include_once '../PHP/Cliente.class.php';
     $cliente = new Cliente();
-    $cliente->setNew_username($_POST['new_username']);
-    $cliente->setNew_email($_POST['new_email']);
-    $cliente->setNew_senha($_POST['new_senha']);
+    $cliente->setNome($_POST['new_username']);
+    $cliente->setEmail($_POST['new_email']);
+    $cliente->setSenha($_POST['new_senha']);
     $cliente->save();
     
     header('Location: ../HTML/cadastro.html');
