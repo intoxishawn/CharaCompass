9a880569027bbbf2602540741a1b6a7628a73263
Larissa Nicolini de Oliveira <02150129@aluno.canoas.ifrs.edu.br>
2023-08-09 13:37:48 -0300

    Hash tirado

--- CharaCompass/PHP/cadastrarCliente.php
+++ CharaCompass/PHP/cadastrarCliente.php
@@ -1,14 +1,12 @@
 <?php
 $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
-$senha = $_POST['new_senha'];
-$hashedSenha = password_hash($senha, PASSWORD_DEFAULT); //usar password_verify() no login
 
 if ($acao === 'cadastrar') {
     include_once '../PHP/Cliente.class.php';
     $cliente = new Cliente();
     $cliente->setNome($_POST['new_username']);
     $cliente->setEmail($_POST['new_email']);
-    $cliente->setSenha($hashedSenha);
+    $cliente->setSenha($_POST['new_senha']);
     $cliente->save();
 
     
