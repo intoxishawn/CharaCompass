1a798af32a27e93e8d4041e494e39d55714ed93d
Larissa Nicolini de Oliveira <02150129@aluno.canoas.ifrs.edu.br>
2023-09-25 17:19:39 -0300

    Cria o objeto, mas não salva no banco

--- CharaCompass/Controller/cadastrarCliente.php
+++ CharaCompass/Controller/cadastrarCliente.php
@@ -2,14 +2,12 @@
 $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
 
 if ($acao === 'cadastrar') {
-    include_once '/CharaCompass/Model/Cliente.class.php';
+    include_once '../Model/Cliente.class.php';
     $cliente = new Cliente();
     $cliente->setNome($_POST['new_username']);
     $cliente->setEmail($_POST['new_email']);
     $cliente->setSenha($_POST['new_senha']);
     $cliente->save();
-
-    header('Location: /CharaCompass/View/cadastro.html');
     exit();
 }
 ?>
--- CharaCompass/Model/Cliente.class.php
+++ CharaCompass/Model/Cliente.class.php
@@ -1,77 +1,76 @@
-<?php
-include_once 'Controller/Conexao.php';
+    <?php
+    include_once '../Controller/Conexao.php';
+    include_once 'Model.trait.php';
 
-class Cliente{
+    class Cliente{
 
-    use Model;
-    private $id;
-    private $nome;
-    private $email;
-    private $senha;
-    private $biografia;
+        use Model;
+        private $id;
+        private $nome;
+        private $email;
+        private $senha;
+        private $biografia;
 
-    //get e set no trait
+        //get e set no trait
 
-   //save
-    public function save()
-    {
-        $pdo = conexao();
-        try {
-            $stmt1 = $pdo->prepare('INSERT INTO cliente (nome_cliente, email_cliente, senha_cliente, biografia_cliente) VALUES(:nome, :email, :senha, :biografia)');
-            $stmt1->execute([
-                ':nome' => $this->nome,
-                ':email' => $this->email,
-                ':senha' => $this->senha,
-                ':biografia' => $this->biografia
-            ]); 
-            $this->id = $pdo->lastInsertId();
-            return true;
-        } catch(Exception $e) { 
-            return false;
+    //save
+        public function save()
+        {
+            $pdo = conexao();
+            try {
+                $stmt1 = $pdo->prepare('INSERT INTO cliente (nome_cliente, email_cliente, senha_cliente) VALUES(:nome, :email, :senha)');
+                $stmt1->execute([
+                    ':nome' => $this->nome,
+                    ':email' => $this->email,
+                    ':senha' => $this->senha
+                ]);
+                return true;
+            } catch(Exception $e) { 
+                return false;
+            }
+        }
+
+        //update
+        public function update(){
+            $pdo = conexao();
+            try{
+                $stmt = $pdo ->prepare('UPDATE cliente SET nome_cliente = :nome, email_cliente = :email, senha_cliente = :senha, 
+                biografia_cliente = :biografia WHERE id_cliente = :id');
+                $stmt->execute([
+                    ':nome' => $this->nome,
+                    ':email' => $this->email,
+                    ':senha' => $this->senha,
+                    ':biografia' => $this->biografia,
+                    ':id' => $this->id
+                ]);
+                return true;
+            }catch(Exception $e){
+                return false;
+            }
+        }
+
+        //delete
+        public static function delete($id){
+            $pdo = conexao();
+            $stmt = $pdo->prepare('DELETE FROM cliente WHERE id = :id');
+            $stmt -> execute([':id'=>$id]);
+        }
+
+        //getAll
+        public static function getAll(){
+            $pdo = conexao();
+            $lista = [];
+            foreach ($pdo->query('SELECT * FROM cliente') as $linha){
+                $cliente = new Cliente();
+                $cliente-> setId($linha['id_cliente']);
+                $cliente->setNome($linha['nome_cliente']);
+                $cliente->setEmail($linha['email_cliente']);
+                $cliente->setSenha($linha['senha_cliente']);
+                $cliente->setBiografia($linha['biografia_cliente']);
+
+                $lista[] = $cliente;
+            } return $lista;
         }
     }
 
-    //update
-    public function update(){
-        $pdo = conexao();
-        try{
-            $stmt = $pdo ->prepare('UPDATE cliente SET nome_cliente = :nome, email_cliente = :email, senha_cliente = :senha, 
-            biografia_cliente = :biografia WHERE id_cliente = :id');
-            $stmt->execute([
-                ':nome' => $this->nome,
-                ':email' => $this->email,
-                ':senha' => $this->senha,
-                ':biografia' => $this->biografia,
-                ':id' => $this->id
-            ]);
-            return true;
-        }catch(Exception $e){
-            return false;
-        }
-    }
-
-    //delete
-    public static function delete($id){
-        $pdo = conexao();
-        $stmt = $pdo->prepare('DELETE FROM cliente WHERE id = :id');
-        $stmt -> execute([':id'=>$id]);
-    }
-
-    //getAll
-    public static function getAll(){
-        $pdo = conexao();
-        $lista = [];
-        foreach ($pdo->query('SELECT * FROM cliente') as $linha){
-            $cliente = new Cliente();
-            $cliente-> setId($linha['id_cliente']);
-            $cliente->setNome($linha['nome_cliente']);
-            $cliente->setEmail($linha['email_cliente']);
-            $cliente->setSenha($linha['senha_cliente']);
-            $cliente->setBiografia($linha['biografia_cliente']);
-
-            $lista[] = $cliente;
-        } return $lista;
-    }
-}
-
-?>
+    ?>
