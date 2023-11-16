556054c0a267f09e1fcd62c1a42dcd6907e44590
Shawn <99032697+intoxishawn@users.noreply.github.com>
2023-11-16 01:59:58 -0300

    bootstrap

--- CharaCompass/View/addMundo.php
+++ CharaCompass/View/addMundo.php
@@ -17,6 +17,7 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <link rel="icon" type="image/x-icon" href="../View/Imagens/favicon-32x32.png">
